c58d12c6e14696584bfc90619eeba6d8631c0b5e
Prahalad Belavadi <prahalad@belavadi.com>
2019-07-10 16:41:50 +0530

    alert service since there'll be more than one alert
    Signed-off-by: Prahalad Belavadi <prahalad@belavadi.com>

--- daily-snap/src/app/services/simple-alert.service.ts
+++ daily-snap/src/app/services/simple-alert.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { AlertController } from "@ionic/angular";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SimpleAlertService {
 
-  constructor() { }
+  constructor(private alertCtrl: AlertController) { }
+
+  createAlert(title: string, message: string): Promise<any> {
+    return this.alertCtrl.create({
+      header: title,
+      message: message,
+      buttons: [
+        {
+          text: 'Ok'
+        }
+      ]
+    });
+  }
+
 }
