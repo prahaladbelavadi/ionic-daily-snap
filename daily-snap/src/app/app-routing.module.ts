7e4f0b5bb99987d3169b3b4da6b20ffe96488ebf
Prahalad Belavadi <prahalad@belavadi.com>
2019-07-10 11:57:51 +0530

    slideshow will be shown as modal
    Signed-off-by: Prahalad Belavadi <prahalad@belavadi.com>

--- daily-snap/src/app/app-routing.module.ts
+++ daily-snap/src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'slideshow', loadChildren: './slideshow/slideshow.module#SlideshowPageModule' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' }
 ];
 
 @NgModule({
