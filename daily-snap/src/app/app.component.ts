b1fa2c5dc06d55d7a4b502a3bc0403b4db65a66d
Prahalad Belavadi <prahalad@belavadi.com>
2019-07-11 13:32:16 +0530

    add local notification scheduler
    Signed-off-by: Prahalad Belavadi <prahalad@belavadi.com>

--- daily-snap/src/app/app.component.ts
+++ daily-snap/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
+import { Platform } from "@ionic/angular";
 
 import { Plugins } from "@capacitor/core";
 const { SplashScreen, StatusBar } = Plugins;
+
+import { LocalNotifications, ELocalNotificationTriggerUnit } from '@ionic-native/local-notifications/ngx';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  constructor() {
+  constructor(private platform: Platform, private localNotifications: LocalNotifications) {
     SplashScreen.hide().catch((err) => {
       console.log(err)
     });
@@ -17,6 +20,25 @@ export class AppComponent {
       console.warn(err);
     });
 
+    if (this.platform.is('cordova')) {
+      this.localNotifications.isScheduled(1).then((scheduled) => {
+        if (!scheduled) {
+          let firstNotificationTime = new Date();
+
+          firstNotificationTime.setHours(firstNotificationTime.getHours() + 24);
+
+          this.localNotifications.schedule({
+            id: 1,
+            title: 'Snapaday',
+            text: 'Have you taken your snap for today ?',
+            trigger: {
+              at: firstNotificationTime,
+              every: ELocalNotificationTriggerUnit.DAY
+            }
+          });
+        }
+      });
+    }
   }
 
 }
